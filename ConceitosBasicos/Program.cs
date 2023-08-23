fb791f33409d880d6618b5d04aea9a4b67f42091
rosenclever.gazoni <rosenclever.gazoni@TPCP04LAB1000>
2023-08-23 20:08:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num1 = 10;
+int num2 = 12;
+int soma = num1 + num2;
+Console.WriteLine($"A soma de {num1} com {num2} é {soma}");
